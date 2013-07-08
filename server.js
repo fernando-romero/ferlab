25469453bab0c8e967d29852ec4982748990cfc1
Fernando Romero <fernando.romero@rightscale.com>
2013-07-07 20:47:04 -0500

    change to listen

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var port = 80;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(port, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:' + port + '/');
+}).listen(port);
+console.log('Server running at port:' + port);
