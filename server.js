e4c5ce856ddc527eda81bc081e729fe786e24d69
Fernando Romero <fernando.romero@rightscale.com>
2013-07-07 20:49:58 -0500

    message changed

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 var port = 80;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Coming soon...\n');
 }).listen(port);
 console.log('Server running at port:' + port);
